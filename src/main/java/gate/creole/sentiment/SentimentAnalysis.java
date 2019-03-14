6f1070c38b3f199971b58901b725bd11b7e6dd21
Mark A. Greenwood <m.a.greenwood@sheffield.ac.uk>
2019-03-14 10:21:59 +0000

    a pre-packaged app class

---
+++ src/main/java/gate/creole/sentiment/SentimentAnalysis.java
@@ -0,0 +1,17 @@
+package gate.creole.french;
+
+import gate.creole.PackagedController;
+import gate.creole.metadata.AutoInstance;
+import gate.creole.metadata.AutoInstanceParam;
+import gate.creole.metadata.CreoleResource;
+
+@CreoleResource(name = "Genric English Sentiment Analysis",
+    comment = "Ready-made English Sentiment Analysis application",
+    autoinstances = @AutoInstance(parameters = {
+	@AutoInstanceParam(name="pipelineURL", value="resources/application.xgapp"),
+	@AutoInstanceParam(name="menu", value="Sentiment")}))
+public class SentimentAnalysis extends PackagedController {
+
+  private static final long serialVersionUID = 5965895217733412732L;
+
+}
